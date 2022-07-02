e01ceadf4403307de748ffd782659ba315636234
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-07-02 05:06:45 -0300

    login finalizado y agregado de dashboard.html plantilla

---
+++ login.js
@@ -0,0 +1,40 @@
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const cartelError = document.getElementById('cartelError');
+const btnSubmit = document.getElementById('btnSubmit');
+btnSubmit.addEventListener('click' ,async(e)=>{
+    e.preventDefault()
+    if(await peticionFetch()){      
+        cartelError.classList = "correcto";
+        setTimeout(() => {
+            location.replace('dashboard.html')
+        }, 1500);
+    }
+    else{
+        cartelError.classList = "error";
+        setTimeout(() => {
+            cartelError.classList = "hidden";
+        }, 1200);
+    }
+})
+async function peticionFetch(){
+    try {    
+        const resp = await fetch("https://basic-server-one.vercel.app/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: `${txtEmail.value}`,
+                password: `${txtPassword.value}`
+            })
+        })
+        const data =await resp.json();
+        if(data.success == false)throw new Error('Los datos no son validos');        
+        cartelError.innerHTML = "INICIANDO";
+        return true;
+    } catch (error) {
+        cartelError.innerHTML = error;
+        return false;
+    }
+}
